8bf19decd1d226db3a5c492d8eb58ff8c3fda5e7
Inder Panesar <InderP@apadmi.com>
2021-02-28 19:15:23 +0000

    Scaled UI, Web specific app bar, image grows on web.

--- basketball_stats_flutter/lib/ui/screens/players/player_stats_page.dart
+++ basketball_stats_flutter/lib/ui/screens/players/player_stats_page.dart
@@ -1,47 +1,78 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:basketball_stats_flutter/ui/utils/AppColors.dart';
 import 'package:basketball_stats_flutter/ui/utils/NetworkLinks.dart';
 import 'package:beamer/beamer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 
-class PlayerStatsPage extends StatelessWidget {
-
+class PlayerStatsPage extends StatefulWidget {
   static final beamLocation = BeamPage(key: ValueKey('player'), child: PlayerStatsPage());
   static final path = '/player';
+
+  @override
+  PlayerStatsPageState createState() => PlayerStatsPageState();
+}
+
+class PlayerStatsPageState extends State<PlayerStatsPage> {
+
+
+  final subheadingStats = AutoSizeGroup();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: false,
       backgroundColor: AppColors.porcelain,
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: AppColors.shark, //change your color here
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
+      appBar: getPlatformSpecificAppBar(),
       body: CustomScrollView(
         slivers: [
           SliverFillRemaining(
             hasScrollBody: false,
             child: Container(
               margin: EdgeInsets.fromLTRB(10,20,20,20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child:
+
+              Column(
+
                 children: [
-                  getTopName(context),
+                  Container(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.topLeft,
+                    child: Stack(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            getTopName(context),
+                          ],
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(top: 110 * scaleValueHeight(), left: 20 * scaleValueWidth()),
+                            child: FadeInImage.memoryNetwork(
+                                placeholder: kTransparentImage,
+                                image: NetworkLinks.dennisSchroderImageLink,
+                                width: MediaQuery.of(context).size.width/1.7,
+                              ),
+
+                        )
+                      ],
+                    ),
+                  ),
+
                   SizedBox(height: 10,),
+
                   Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      //ToDo: Make bio widget after implementing api calls.
-                      child: SelectableText("Bio", style: TextStyle( fontSize: 40, color: AppColors.shark, fontFamily: 'Bw Beto Grande')),
+                    padding: EdgeInsets.only(left: 20),
+                    //ToDo: Make bio widget after implementing api calls.
+                    child: SelectableText("Bio", style: TextStyle( fontSize: 40, color: AppColors.shark, fontFamily: 'Bw Beto Grande')),
                   )
                 ],
-              ),
+              )
+
             ),
 
           ),
@@ -51,32 +82,50 @@ class PlayerStatsPage extends StatelessWidget {
   }
 
 
+
+
   Widget getTopName(BuildContext context) {
     return Stack(
       children: [
+
+
         Row(
           children: [
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  SelectableText("Dennis\nSchröder", style: TextStyle( fontSize: 70, color: AppColors.shark, fontFamily: 'Bw Beto Grande')),
-                  SizedBox(height: 10,),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text(
+                          "Giannis\nAntetokounmpo",
+                          style: TextStyle(
+                            color: AppColors.shark,
+                            fontFamily: 'Bw Beto Grande',
+                            fontSize: 70,
+                          ),
+                          maxLines: 2,
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
                   Container(
                     width: MediaQuery.of(context).size.width - (MediaQuery.of(context).size.width/1.7) - 50,
                     child: Padding(
-                      padding: EdgeInsets.only(left:20),
+                      padding: EdgeInsets.only(left:10),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          SelectableText("14.5", style: TextStyle( fontSize: 40, color: AppColors.shark, fontFamily: 'Bw Beto Grande', letterSpacing: -1.5, fontWeight: FontWeight.w600)),
-                          SelectableText("Points Per Game", style: TextStyle( fontSize: 10, color: AppColors.shark, fontFamily: 'Bw Beto Grande')),
+                          statTitle("14.5"),
+                          statSubtitle("Points Per Game", "PPG"),
                           SizedBox(height: 20,),
-                          SelectableText("3.6", style: TextStyle( fontSize: 40, color: AppColors.shark, fontFamily: 'Bw Beto Grande', letterSpacing: -1.5, fontWeight: FontWeight.w600)),
-                          SelectableText("Rebounds Per Game", style: TextStyle( fontSize: 10, color: AppColors.shark, fontFamily: 'Bw Beto Grande')),
+                          statTitle("3.6"),
+                          statSubtitle("Rebounds Per Game", "RPG"),
                           SizedBox(height: 20,),
-                          SelectableText("4.2", style: TextStyle( fontSize: 40, color: AppColors.shark, fontFamily: 'Bw Beto Grande', letterSpacing: -1.5, fontWeight: FontWeight.w600)),
-                          SelectableText("Assists Per Game", style: TextStyle( fontSize: 10, color: AppColors.shark, fontFamily: 'Bw Beto Grande')),
+                          statTitle("4.2"),
+                          statSubtitle("Assists Per Game", "APG"),
                           SizedBox(height: 20,),
                         ],
                       ),
@@ -88,17 +137,119 @@ class PlayerStatsPage extends StatelessWidget {
           ],
         ),
 
-        Positioned(
-            top: 140,
-            left: 20,
-            child: FadeInImage.memoryNetwork(
-                placeholder: kTransparentImage,
-                image: NetworkLinks.dennisSchroderImageLink,
-                width: MediaQuery.of(context).size.width/1.7
-            )
-        )
+
       ],
     );
   }
+
+  double scaleValueWidth() {
+     return MediaQuery.of(context).size.width/500.0;
+  }
+
+  double scaleValueHeight() {
+    return MediaQuery.of(context).size.height/919.0;
+  }
+
+
+  Widget statTitle(String title) {
+    return SizedBox(
+      height: 40,
+      child: Row(
+        children: [
+          Expanded(
+            child: AutoSizeText(
+                title,
+                minFontSize: 30,
+                maxLines: 1,
+                style: TextStyle(
+                    fontSize: 40,
+                    color: AppColors.shark,
+                    fontFamily: 'Bw Beto Grande',
+                    letterSpacing: -1.5,
+                    fontWeight: FontWeight.w600
+                )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+  Widget statSubtitle(String subtitle, String abbreviate) {
+    return SizedBox(
+      height: 12,
+      child: Row(
+        children: [
+          Expanded(
+            child: AutoSizeText(
+                subtitle,
+                minFontSize: 10,
+                group: subheadingStats,
+                overflowReplacement: Text(
+                    abbreviate,
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: AppColors.shark,
+                        fontFamily: 'Bw Beto Grande'
+                    ) ,
+                ),
+                maxLines: 1,
+                style: TextStyle(
+                    fontSize: 12,
+                    color: AppColors.shark,
+                    fontFamily: 'Bw Beto Grande'
+                )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+  Widget getPlatformSpecificAppBar() {
+    if (kIsWeb) {
+      return AppBar(
+        automaticallyImplyLeading: false,
+        leading: Navigator.canPop(context) ? TextButton(
+            child:  Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: FittedBox(
+                  child: TextButton(
+                    child: Text(
+                    "Back",
+                    style: TextStyle(
+                        color: AppColors.shark,
+                        fontFamily: 'Bw Beto Grande',
+                        fontSize: 70),
+                    maxLines: 1,
+                  ),
+                  onPressed: () => Navigator.pop(context),
+            ),
+                )
+
+            )
+
+        ) : null,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      );
+    }
+    else {
+      return AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: AppColors.shark, //change your color here
+        ),
+      );
+    }
+  }
+
+
+
+
+
 }
 
